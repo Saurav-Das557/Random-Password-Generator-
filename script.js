c0578b8769a87d9269933e142a6a3df13569a9f8
Saurav Das <sauravdas557@gmail.com>
2024-12-17 13:30:37 +0600

    made changes

--- script.js
+++ script.js
@@ -1,39 +1,86 @@
-const passwordBox= document.getElementById("password");
+const inputSlider = document.getElementById("inputSlider");
+const sliderValue = document.getElementById("sliderValue");
+const passBox = document.getElementById("passBox");
 
-const length=16;
-const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const lowerCase="abcdefghijklmnopqrstuvwxyz"
-const number= "0123456789";
-const symbols=" @#$^&*()_+~|{}[]<>/-="
-const allChars=upperCase+lowerCase+number+symbols
+const lowercaseEl = document.getElementById("lowercase");
+const uppercseEl = document.getElementById("uppercase");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
 
-function createPassword(){
-    let password="";
-    password +=upperCase[Math.floor(Math.random()*upperCase.length)]
-    password +=lowerCase[Math.floor(Math.random()*lowerCase.length)]
-    password +=number[Math.floor(Math.random()*number.length)]
-    password +=symbols[Math.floor(Math.random()*symbols.length)]
+const generateBtn = document.getElementById("genBtn");
+const copyBtn = document.getElementById("copyIcon");
+const passIndicator = document.getElementById("passIndicator");
 
-    while(length>password.length){
-        password +=allChars[Math.floor(Math.random()*allChars.length)]
 
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-=[]{}\\|;':\",./<>?";
+
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener("input",()=>{
+
+    sliderValue.textContent = inputSlider.value;
+    generatePassword();
+
+});
+
+function generatePassword(){
+    const length = inputSlider.value;
+    let characters = "";
+    let password = "";
+
+    characters += lowercaseEl.checked ? lowercaseLetters : "";
+    characters += uppercseEl.checked ? uppercaseLetters : "";
+    characters += numbersEl.checked ? numbers : "";
+    characters += symbolsEl.checked ? symbols : "";
+
+    for(let i = 0 ; i < length ; i++){
+       password += characters.charAt(Math.floor(Math.random() * characters.length));
     }
-    passwordBox.value=password;
+
+    passBox.value = password;
+    updatePasswordIndicator();
+
 }
 
-function copyPassword() {
-    if (passwordBox.value !== "") {
-        passwordBox.select();
-        navigator.clipboard.writeText(passwordBox.value)
-            .then(() => {
-                alert("Password copied to clipboard!");
-            })
-            .catch(err => {
-                alert("Failed to copy the password. Try again!");
-                console.error("Error copying password: ", err);
-            });
-    } else {
-        alert("Please generate a password first!");
+generateBtn.addEventListener("click",()=>{
+    generatePassword();
+});
+
+function updatePasswordIndicator(){
+    const passwordStrength = getPasswordStrength(passBox.value);
+    console.log(passwordStrength);
+    passIndicator.className = "pass-indicator " + passwordStrength;
+    console.log(passIndicator.className);
+}
+
+function getPasswordStrength(password){
+
+    if(password.length <=10){
+        return "weak";
+    }else if (password.length <=20){
+        return "medium";
+    }else{
+        return "strong";
     }
 }
 
+
+window.addEventListener('DOMContentLoaded',()=>{
+    updatePasswordIndicator();
+});
+
+
+copyBtn.addEventListener("click",()=>{
+
+    if(passBox.value != "" || passBox.value.length >= 1){
+        navigator.clipboard.writeText(passBox.value);
+        copyBtn.innerText = "check";
+
+        setTimeout(()=>{
+            copyBtn.innerHTML = "content_copy";
+        },3000);
+    }
+
+});
